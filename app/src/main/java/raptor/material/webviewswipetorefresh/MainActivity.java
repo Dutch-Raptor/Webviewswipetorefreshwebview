700562170f11e64117a0fb0d1d11763a244d5e60
Dutch-Raptor <scoutlion1990@gmail.com>
2015-03-21 17:21:32 +0100

    webviewswipetorefresh

---
+++ app/src/main/java/raptor/material/webviewswipetorefresh/MainActivity.java
@@ -0,0 +1,59 @@
+package raptor.material.webviewswipetorefresh;
+
+import android.os.Handler;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final WebView wv = (WebView) findViewById(R.id.webView);
+        wv.loadUrl("http://www.reddit.com/r/materialdesign/comments/2zqq24/_/");
+        final SwipeRefreshLayout swiptorefreshlayout = (SwipeRefreshLayout) findViewById(R.id.swiptorefresh);
+        swiptorefreshlayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+
+                final Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        swiptorefreshlayout.setRefreshing(false);
+                        wv.reload();
+                    }
+                }, 900);
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
